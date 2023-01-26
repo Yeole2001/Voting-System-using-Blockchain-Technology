417db12a7d41973a97f2d0e4d969d66f405842c1
Atharv Yeole <atharv0015@gmail.com>
2023-01-26 08:58:33 +0530

    All code transferred

---
+++ client/src/component/Footer/Footer.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+import "./Footer.css";
+
+const Footer = () => (
+  <>
+    <div className="footer-block"></div>
+    <div className="footer">
+      <div className="footer-container">
+        <p>
+          Made with <i className="fas fa-heartbeat" /> by Sujata Atharv Asmita Sakshi
+        </p>
+      </div>
+    </div>
+  </>
+);
+
+
+
+export default Footer;
